bc45700e5e9a6c1f21fe9a11032464ef54e0c40f
Shady Mostafa <shady.mostafa@sporty.com>
2025-05-06 16:14:21 +0200

    Initial Commit

---
+++ DependabotTestTests/DependabotTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  DependabotTestTests.swift
+//  DependabotTestTests
+//
+//  Created by Shady Mostafa on 06.05.25.
+//
+
+import Testing
+@testable import DependabotTest
+
+struct DependabotTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
